1a8dcef8fdbb1dbe8d3da81f1ed2b67a68c166a3
mchelliah1 <mchelliah01@gmail.com>
2025-05-11 17:29:08 -0700

    Initial Commit

---
+++ DineTime/DineTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DineTimeApp.swift
+//  DineTime
+//
+//  Created by Matthew Chelliah on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DineTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
